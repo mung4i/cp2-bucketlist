80d01c948fddda75f6f5f28c520e841e8ad67e90
mungaiandela <martin.mungai@andela.com>
2017-04-11 13:13:41 +0300

    [Feature] Made changes to accomodate production configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
+import os
+
+
 from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -6,13 +11,30 @@ from config import app_config
 
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.environ['SECRET_KEY'],
+            SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
+
     db.init_app(app)
+
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to have access"
+    login_manager.login_view = "auth.login"
+
+    migrate = Migrate(app, db)
+
+    from app import models
 
     @app.route('/')
     def hello_world():
