71dd6b028285d6a47303e93f19c7d8f21a003f7f
mungaiandela <martin.mungai@andela.com>
2017-04-11 09:48:23 +0300

    [Feature] Add testing config

--- config.py
+++ config.py
@@ -1,11 +1,23 @@
 # Enable debugging features
-DEBUG = True
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     """
     My configs
     """
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'the-secret-secret-k3y'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_ECHO = True
 
 
 class DevelopmentConfig(Config):
@@ -15,6 +27,7 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_ECHO = False
 
 
 app_config = {
