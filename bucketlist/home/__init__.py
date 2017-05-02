fada4f2c46ff25df6f91dddb7d9bd5041ad13256
mungaiandela <martin.mungai@andela.com>
2017-05-02 11:19:57 +0300

    [Feature] Add bucketlist api endpoints for POST, GET, PUT, DELETE

--- bucketlist/home/__init__.py
+++ bucketlist/home/__init__.py
@@ -4,4 +4,18 @@ from flask import Blueprint
 home = Blueprint('home', __name__)
 
 
-# from . import views
+from . import views
+
+
+bucketlist_view = views.BucketlistAPI.as_view('bucketlist_api')
+
+home.add_url_rule(
+    '/v1/bucketlists/',
+    view_func=bucketlist_view,
+    methods=['POST', 'GET']
+)
+home.add_url_rule(
+    '/v1/bucketlists/<int:id>',
+    view_func=bucketlist_view,
+    methods=['PUT', 'DELETE', 'GET']
+)
