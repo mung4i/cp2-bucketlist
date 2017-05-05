e57517a0242201ef0acbf10b5a5b594ca621f595
mungaiandela <martin.mungai@andela.com>
2017-05-05 15:40:22 +0300

    [Chore] Remove try except clauses from views

--- bucketlist/home/views.py
+++ bucketlist/home/views.py
@@ -34,31 +34,23 @@ class BucketlistAPI(MethodView):
             return make_response(jsonify(response)), 400
 
         if not bucketlist:
-            try:
-                email = User.decode_auth_token(headers)
-                user = User.query.filter_by(email=email).first()
-                create = Bucketlist(
-                    title=data.get('title'),
-                    date_created=self.now,
-                    date_modified=self.now,
-                    users_email=user.email)
+            email = User.decode_auth_token(headers)
+            user = User.query.filter_by(email=email).first()
+            create = Bucketlist(
+                title=data.get('title'),
+                date_created=self.now,
+                date_modified=self.now,
+                users_email=user.email)
 
-                db.session.add(create)
-                db.session.commit()
+            db.session.add(create)
+            db.session.commit()
 
-                response = {
-                    'status': 'Success',
-                    'message': 'Bucketlist has been created'
-                }
+            response = {
+                'status': 'Success',
+                'message': 'Bucketlist has been created'
+            }
 
-                return make_response(jsonify(response)), 201
-            except Exception as e:
-                print(e)
-                response = {
-                    'status': 'Fail',
-                    'message': 'Some error occurred.'
-                }
-                return make_response(jsonify(response)), 400
+            return make_response(jsonify(response)), 201
         else:
             response = {
                 'status': 'Fail',
@@ -77,6 +69,7 @@ class BucketlistAPI(MethodView):
         parser.add_argument(
             'page', type=int, required=False, location='args')
         args = parser.parse_args()
+
         email = User.decode_auth_token(headers)
         user = User.query.filter_by(email=email).first()
 
@@ -114,7 +107,7 @@ class BucketlistAPI(MethodView):
                     users_email=user.email).paginate(
                     page=page, per_page=limit, error_out=True)
 
-            if not bucketlists:
+            if not bucketlists.items:
                 response = {
                     'status': 'Fail',
                     'message': 'You do not have bucketlists'
@@ -162,27 +155,19 @@ class BucketlistAPI(MethodView):
     def delete(self, id):
         headers = request.headers.get('Authorization')
         if headers:
-            try:
-                email = User.decode_auth_token(headers)
-                user = User.query.filter_by(email=email).first()
-                bucketlists = Bucketlist.query.filter_by(
-                    users_email=user.email).all()
-                for bucketlist in bucketlists:
-                    if id == bucketlist.id:
-                        db.session.delete(bucketlist)
-                        db.session.commit()
-                response = {
-                    'status': "Success",
-                    'message': 'Deleted'
-                }
-                return make_response(jsonify(response)), 204
-
-            except:
-                response = {
-                    'status': 'Fail',
-                    'message': 'Some error occurred'
-                }
-                return make_response(jsonify(response)), 400
+            email = User.decode_auth_token(headers)
+            user = User.query.filter_by(email=email).first()
+            bucketlists = Bucketlist.query.filter_by(
+                users_email=user.email).all()
+            for bucketlist in bucketlists:
+                if id == bucketlist.id:
+                    db.session.delete(bucketlist)
+                    db.session.commit()
+            response = {
+                'status': "Success",
+                'message': 'Deleted'
+            }
+            return make_response(jsonify(response)), 204
         else:
             response = {
                 'status': 'Fail',
@@ -195,37 +180,28 @@ class BucketlistAPI(MethodView):
         data = request.get_json()
         headers = request.headers.get('Authorization')
         if headers:
-            try:
-                email = User.decode_auth_token(headers)
-                user = User.query.filter_by(email=email).first()
-                bucketlists = Bucketlist.query.filter_by(
-                    users_email=user.email).all()
+            email = User.decode_auth_token(headers)
+            user = User.query.filter_by(email=email).first()
+            bucketlists = Bucketlist.query.filter_by(
+                users_email=user.email).all()
 
-                for bucketlist in bucketlists:
-                    if user.email == bucketlist.users_email:
-                        bucketlist.title = data.get('title')
-                        bucketlist.date_modified = self.now
-                        db.session.commit()
-                    response = {
-                        'status': "Success",
-                        'message': "Bucketlist has been updated"
-                    }
-                    return make_response(jsonify(response)), 200
-
-                else:
-                    response = {
-                        'status': 'Fail',
-                        'message':
-                        'You are not authorized to update these resources'
-                    }
-                    return make_response(jsonify(response)), 401
-
-            except:
+            for bucketlist in bucketlists:
+                if user.email == bucketlist.users_email:
+                    bucketlist.title = data.get('title')
+                    bucketlist.date_modified = self.now
+                    db.session.commit()
+                response = {
+                    'status': "Success",
+                    'message': "Bucketlist has been updated"
+                }
+                return make_response(jsonify(response)), 200
+            else:
                 response = {
                     'status': 'Fail',
-                    'message': 'Some error occured.'
+                    'message':
+                    'You are not authorized to update these resources'
                 }
-                return make_response(jsonify(response)), 400
+                return make_response(jsonify(response)), 401
 
 
 class BucketListItemsAPI(MethodView):
@@ -247,95 +223,77 @@ class BucketListItemsAPI(MethodView):
                 'message': "Bucketlist name is missing"
             }
             return make_response(jsonify(response)), 400
-        try:
-            if not items:
-                email = User.decode_auth_token(headers)
-                user = User.query.filter_by(email=email).first()
-                if User.decode_auth_token(user.email):
-                    create = Items(
-                        name=data.get('name'),
-                        date_created=self.now,
-                        date_modified=self.now,
-                        done=False,
-                        bucketlist_id=id)
-                    db.session.add(create)
-                    db.session.commit()
+        if not items:
+            email = User.decode_auth_token(headers)
+            user = User.query.filter_by(email=email).first()
+            if User.decode_auth_token(user.email):
+                create = Items(
+                    name=data.get('name'),
+                    date_created=self.now,
+                    date_modified=self.now,
+                    done=False,
+                    bucketlist_id=id)
+                db.session.add(create)
+                db.session.commit()
 
-                response = {
-                    'status': 'Success',
-                    'message': 'Bucketlist item has been created'
-                }
+            response = {
+                'status': 'Success',
+                'message': 'Bucketlist item has been created'
+            }
 
-                return make_response(jsonify(response)), 201
+            return make_response(jsonify(response)), 201
 
-            else:
-                response = {
-                    'status': 'Fail',
-                    'message': 'Bucketlist already exists.'
-                }
-                return make_response(jsonify(response)), 403
-
-        except:
+        else:
             response = {
                 'status': 'Fail',
-                'message': 'You are not authorized to view these resources'
+                'message': 'Bucketlist already exists.'
             }
-            return make_response(jsonify(response)), 401
+            return make_response(jsonify(response)), 403
 
     @jwt_required()
     def get(self, id, item_id=None):
-        try:
-            headers = request.headers.get('Authorization')
-            email = User.decode_auth_token(headers)
-            user = User.query.filter_by(email=email).first()
-            if item_id:
-                item = Items.query.filter_by(
-                    id=item_id).first()
-                if not item:
-                    response = {
-                        'status': 'Fail',
-                        'message': 'The bucketlist item does not exist'
-                    }
-                    return make_response(jsonify(response)), 404
-                if item and user:
-                    response = {
-                        'name': item.name,
-                        'date_created': item.date_created,
-                        'date_modified': item.date_modified,
-                        'done': item.done,
-                        'bucketlist_id': item.bucketlist_id
-                    }
-                    return make_response(jsonify(response)), 200
-            if id:
-                all_items = []
-                items = Items.query.filter_by(
-                    bucketlist_id=id).all()
-                if not items:
-                    response = {
-                        "status": "Fail",
-                        "message": "You do not have any bucketlist items"
-                    }
-
-                    return make_response(jsonify(response)), 404
-                for item in items:
-                    response = {
-                        "name": item.name,
-                        "date_created": item.date_created,
-                        "date_modified": item.date_modified,
-                        "done": item.done,
-                        "bucketlist_id": item.bucketlist_id,
-                        "item_id": item.id
-                    }
-                    all_items.append(response)
-                return make_response(jsonify(all_items)), 200
-        except Exception as e:
-            print(e)
-            response = {
-                'status': 'Fail',
-                'message': 'Some error occurred.'
-            }
-            return make_response(jsonify(response)), 400
-
+        headers = request.headers.get('Authorization')
+        email = User.decode_auth_token(headers)
+        user = User.query.filter_by(email=email).first()
+        if item_id:
+            item = Items.query.filter_by(
+                id=item_id).first()
+            if not item:
+                response = {
+                    'status': 'Fail',
+                    'message': 'The bucketlist item does not exist'
+                }
+                return make_response(jsonify(response)), 404
+            if item and user:
+                response = {
+                    'name': item.name,
+                    'date_created': item.date_created,
+                    'date_modified': item.date_modified,
+                    'done': item.done,
+                    'bucketlist_id': item.bucketlist_id
+                }
+                return make_response(jsonify(response)), 200
+        if id:
+            all_items = []
+            items = Items.query.filter_by(
+                bucketlist_id=id).all()
+            if not items:
+                response = {
+                    "status": "Fail",
+                    "message": "You do not have any bucketlist items"
+                }
+                return make_response(jsonify(response)), 404
+            for item in items:
+                response = {
+                    "name": item.name,
+                    "date_created": item.date_created,
+                    "date_modified": item.date_modified,
+                    "done": item.done,
+                    "bucketlist_id": item.bucketlist_id,
+                    "item_id": item.id
+                }
+                all_items.append(response)
+            return make_response(jsonify(all_items)), 200
         else:
             response = {
                 'status': 'Fail',
@@ -346,53 +304,38 @@ class BucketListItemsAPI(MethodView):
     @jwt_required()
     def put(self, id=None, item_id=None):
         data = request.get_json()
-        try:
-            item = Items.query.filter_by(
-                id=item_id).first()
-            if item.bucketlist_id == id:
-                item.name = data.get('name')
-                item.date_modified = self.now
-                db.session.commit()
-                response = {
-                    'status': "Success",
-                    'message': "Bucketlist has been updated"
-                }
-                return make_response(jsonify(response)), 200
-        except:
+        item = Items.query.filter_by(
+            id=item_id).first()
+        if item:
+            item.name = data.get('name')
+            item.date_modified = self.now
+            db.session.commit()
             response = {
-                'status': 'Fail',
-                'message': 'Some error occured.'
+                'status': "Success",
+                'message': "Bucketlist has been updated"
             }
-            return make_response(jsonify(response)), 400
+            return make_response(jsonify(response)), 200
 
         else:
             response = {
                 'status': 'Fail',
                 'message':
-                'You are not authorized to update these resources'
+                'The resources you want to update do not exist.'
             }
-            return make_response(jsonify(response)), 401
+            return make_response(jsonify(response)), 400
 
     @jwt_required()
     def delete(self, id=None, item_id=None):
-        try:
-            item = Items.query.filter_by(
-                id=item_id).first()
-            if item.bucketlist_id == id:
-                db.session.delete(item)
-                db.session.commit()
+        item = Items.query.filter_by(
+            id=item_id).first()
+        if item.bucketlist_id == id:
+            db.session.delete(item)
+            db.session.commit()
             response = {
                 'status': "Success",
                 'message': 'Deleted'
             }
             return make_response(jsonify(response)), 204
-
-        except:
-            response = {
-                'status': 'Fail',
-                'message': 'Some error occurred'
-            }
-            return make_response(jsonify(response)), 400
         else:
             response = {
                 'status': 'Fail',
