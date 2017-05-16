5ccd3537bba54b6dd928f06b8dbc8808afefe2f8
mungaiandela <martin.mungai@andela.com>
2017-05-16 13:57:59 +0300

    [Chore] Change url to environment variable

--- bucketlist/home/__init__.py
+++ bucketlist/home/__init__.py
@@ -1,12 +1,11 @@
 from flask import Blueprint, request
-from config import BASE_URL
-
+from flask import current_app as app
 from . import views
 
 
 def add_cors_headers(response):
     response.headers.add(
-        'Access-Control-Allow-Origin', BASE_URL)
+        'Access-Control-Allow-Origin', app.config.get('FRONTEND_URL'))
     if request.method == 'OPTIONS':
         response.headers[
             'Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
