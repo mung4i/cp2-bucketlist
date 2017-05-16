179eddb2a6850138a2f4b0787544054369c790cd
mungaiandela <martin.mungai@andela.com>
2017-05-16 11:51:07 +0300

    [Chore] Change hard coded url string

--- bucketlist/home/__init__.py
+++ bucketlist/home/__init__.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, request
+from config import BASE_URL
 
 from . import views
 
 
 def add_cors_headers(response):
     response.headers.add(
-        'Access-Control-Allow-Origin', 'http://localhost:8000')
+        'Access-Control-Allow-Origin', BASE_URL)
     if request.method == 'OPTIONS':
         response.headers[
             'Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
