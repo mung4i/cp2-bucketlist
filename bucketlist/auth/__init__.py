12737cd307a46b81e1508390ea2b44a13ad92b0a
mungaiandela <martin.mungai@andela.com>
2017-05-17 10:18:51 +0300

    [Chore] Add trailing slash to auth routes

--- bucketlist/auth/__init__.py
+++ bucketlist/auth/__init__.py
@@ -21,13 +21,13 @@ api = Api(auth)
 from . import views
 
 auth.add_url_rule(
-    '/v1/auth/register',
+    '/v1/auth/register/',
     view_func=views.RegisterAPI.as_view('register_api'),
     methods=['POST']
 )
 
 auth.add_url_rule(
-    '/v1/auth/login',
+    '/v1/auth/login/',
     view_func=views.LoginAPI.as_view('login_api'),
     methods=['POST']
 )
